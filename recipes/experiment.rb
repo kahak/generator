5276d656f6822be8d2dbad524443196d5bb8a178
k.hakimzadeh@gmail.com <k.hakimzadeh@gmail.com>
2015-10-09 10:58:32 +0200

    Code generated by Karamel.

---
+++ recipes/experiment.rb
@@ -0,0 +1,17 @@
+#private_ip = my_private_ip()
+#public_ip = my_public_ip()
+
+script 'run_experiment' do
+  cwd "/home/generator"
+   user node['generator']['user']
+  group node['generator']['group']
+  interpreter "bash"
+  code <<-EOM
+rm -f /tmp/input.txt
+
+base64 /dev/urandom | head -c 128000000 > /tmp/input.txt
+
+/srv/hadoop/bin/hdfs dfs -copyFromLocal /tmp/input.txt /words/#{node.name}
+  EOM
+end
+
