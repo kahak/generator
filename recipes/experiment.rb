6f18b788135ee9a60b6fdda3c41055da50d67ac6
Kamal Hakimzadeh <k.hakimzadeh@gmail.com>
2015-10-09 11:43:51 +0200

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -11,6 +11,8 @@ rm -f /tmp/input.txt
 
 base64 /dev/urandom | head -c 128000000 > /tmp/input.txt
 
+/srv/hadoop/bin/hdfs dfs -mkdir -p /words
+
 /srv/hadoop/bin/hdfs dfs -copyFromLocal /tmp/input.txt /words/#{node.name}
   EOM
 end
